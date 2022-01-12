818cb60d0ca03084a3226bd22fe560b65b98d299
Menelaos <mkaskiris@hotmail.com>
2022-01-12 14:55:09 +0000

    page buttons

--- src/RepoMain.js
+++ src/RepoMain.js
@@ -8,6 +8,7 @@ function RepoMain() {
   const [warning, setWarning] = useState()
   const [userData, setUserData] = useState([])
   const [repos, setRepos] = useState([])
+  const [page, setPage] = useState(1)
   const navigate = useNavigate()
 
   const state = useLocation()
@@ -15,7 +16,7 @@ function RepoMain() {
    useEffect(() =>{
      console.log(state.state)
      if(state.state == 'null' || !state.state){
-       setUsername()
+       setUsername('github')
      }else{
        setUsername(state.state);
      }
@@ -26,7 +27,7 @@ function RepoMain() {
     if (username === undefined) return
 
     async function fetchData() {
-      const user = await fetch(`https://api.github.com/users/${username}/repos`)
+      const user = await fetch(`https://api.github.com/users/${username}/repos?page=${page}`)
       const data = await user.json();
 
       if (data.length) {
@@ -48,11 +49,22 @@ function RepoMain() {
     fetchData()
     fetchUserData()
 
-  }, [username])
+  }, [username, page])
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setUsername(e.target.user.value)
+  }
+
+  const getNextPage = () =>{
+    if(page < userData.public_repos / 30)
+      setPage(page=>page+1)
+    //console.log(page)
+  }
+
+  const getPrevPage = () =>{
+    if(page > 1)
+      setPage(page=>page-1)
   }
 
   const repositories = repos.map((repo, i) => {
@@ -66,7 +78,6 @@ function RepoMain() {
 
   return (
     <div className="RepoMain">
-      <h1>State sent {state.state}</h1>
       <form onSubmit={handleSubmit}>
         <input name="user" required placeholder="Enter GitHub username" />
         <input type="submit" />
@@ -83,10 +94,12 @@ function RepoMain() {
       }
 
       <p>{userData.bio}</p>
-
+      <p>This user has {userData.public_repos} public repositories</p>
       <div className="inner-repo">
         {repositories}
       </div>
+      <button onClick={getPrevPage}>Prev</button>
+      <button onClick={getNextPage}>Next</button>
     </div>
   );
 }
