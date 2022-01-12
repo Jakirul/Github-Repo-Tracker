a23d8135c77099d04d1cfa4d75aa0fd80b4fe2d4
Menelaos <mkaskiris@hotmail.com>
2022-01-12 13:39:40 +0000

    d

--- src/RepoMain.js
+++ src/RepoMain.js
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import './RepoMain.css'
 
 function RepoMain() {
@@ -9,6 +9,13 @@ function RepoMain() {
   const [userData, setUserData] = useState([])
   const [repos, setRepos] = useState([])
   const navigate = useNavigate()
+
+  const state = useLocation()
+
+   useEffect(() =>{
+     //console.log(state)
+     setUsername(state.state);
+   }, [])
 
 
   useEffect(() => {
@@ -44,54 +51,32 @@ function RepoMain() {
     setUsername(e.target.user.value)
   }
 
-<<<<<<< HEAD
-  const repositories = (repos) => {
-    return (
-      <div>
-        <ul>
-          {repos.map((repo) => (
-            <li>
-              <h1>{repo.name}</h1>
-              <h3>Number of forks: {repo.forks}</h3>
-              <aside>
-              <h3>{repo.stargazers_count}</h3>
-              </aside>
-              {console.log(repo)}
-              <button onClick={() => navigate(`/${repo.id}`)}>Click here to go to repo page</button>
-            </li>
-          ))}
-        </ul>
-=======
   const repositories = repos.map((repo, i) => {
     return (
       <div key={i}>
         <h2>{repo.name}</h2>
         <button onClick={() => navigate(`/${repo.id}`)}>Repo page</button>
->>>>>>> 2ce85b8af62dcda00aa4a12b131650f502c24593
       </div>
     );
-  }
+  })
 
   return (
     <div className="RepoMain">
+      <h1>State sent {state.state}</h1>
       <form onSubmit={handleSubmit}>
         <input name="user" required placeholder="Enter GitHub username" />
         <input type="submit" />
       </form>
       {warning}
 
-<<<<<<< HEAD
-      {repositories(repos)}
-=======
       {Object.keys(userData).length && !warning ?
         <div className="user-info">
           <h1>{userData.login}</h1>
-          <img src={userData.avatar_url} />
+          <img src={userData.avatar_url} alt='avatar' />
         </div>
         :
         null
       }
->>>>>>> 2ce85b8af62dcda00aa4a12b131650f502c24593
 
       <p>{userData.bio}</p>
 
